8674b757dd5c9fbcdacfcc850cb2177adb8834b1
lacava <williamlacava@gmail.com>
2024-06-26 10:19:30 -0400

    add ci for member pages

---
+++ .github/workflows/make_member_pages.py
@@ -0,0 +1,32 @@
+from ruamel.yaml import YAML
+yaml=YAML(typ='safe', pure=True) 
+import os
+
+with open('_data/authors.yml') as f:
+    authors = yaml.load(f)
+
+for author, v in authors.items():
+    given_name = v['name'].split(' ')[0]
+    family_name = v['name'][len(given_name):]
+    title = v['name']
+    if 'degrees' in v: 
+        title += v['degrees']
+    page = '\n'.join([
+        "---",
+        f"title: {title}",
+        f"header:\n  teaser: \"{v['avatar']}\"",
+        f"author: {author}",
+        f"given_name: {given_name}",
+        f"family_name: {family_name}",
+        "author_profile: true",
+        "---\n",
+        "{{ site.data.authors[page.author].about }}"
+    ])
+    if author == 'bill':
+        page_file = f'_members/01_{family_name.replace(" ","")}-{given_name}.md' 
+    else:
+        page_file = f'_members/{family_name.replace(" ","")}-{given_name}.md' 
+
+    print('writing',page_file,'...')
+    with open(page_file, 'w') as f:
+        f.write(page)
--- _members/01_lacava-william.md
+++
@@ -1,9 +0,0 @@
----
-title: William La Cava
-header: 
-    teaser:  "../assets/images/lacava_profile.JPG"
-author: bill
-author_profile: true
----
-
-{{ site.data.authors[page.author].summary }}
