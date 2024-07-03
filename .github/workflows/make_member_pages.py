0d13a36d0c988976dbfef2c272631e36deccd3cf
lacava <williamlacava@gmail.com>
2024-07-03 15:00:13 -0400

    updates members to list papers, some re-org

--- .github/workflows/make_member_pages.py
+++ .github/workflows/make_member_pages.py
@@ -8,7 +8,31 @@ with open('_data/authors.yml') as f:
 
 shutil.rmtree('_members')
 os.makedirs('_members',exist_ok=True)
+page_content = (
+"""
+{{ site.data.authors[page.author].about }}
 
+{% include pubs_by_author.html given=page.given_name family=page.family_name %}
+{% unless pubs_by_author == empty %}
+  <h2 class="archive__subtitle">Recent Papers </h2> 
+  <div class="entries-list">
+    {% for paper in pubs_by_author  limit: 2 %}
+      {% include pub-single.html pubid=paper%}
+    {% endfor %}
+  </div>
+{% endunless %}
+
+{% assign posts = site.posts | where: "author", page.author %}
+{% unless posts == empty %}
+  <h2 class="archive__subtitle">Recent Posts</h2> 
+  <div class="entries-list">
+    {% for post in posts limit: 2 %}
+      {% include archive-single.html type="list" %}
+    {% endfor %}
+  </div>
+{% endunless %}
+"""
+)
 for author, v in authors.items():
     name = v['name'].split(',')[0]
     given_name = name.split(' ')[0]
@@ -23,7 +47,7 @@ for author, v in authors.items():
         f"family_name: {family_name}",
         "author_profile: true",
         "---\n",
-        "{{ site.data.authors[page.author].about }}"
+        page_content
     ])
     if author == 'bill':
         page_file = f'_members/01_{family_name.replace(" ","")}-{given_name}.md' 
